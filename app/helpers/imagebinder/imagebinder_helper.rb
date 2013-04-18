0b116c01142b11c20b9c684fe057093bd1ca3e9e
Vadym Lukavyy <vadym@masterofcode.com>
2013-04-19 00:43:10 +0300

    Helpers

---
+++ app/helpers/imagebinder/imagebinder_helper.rb
@@ -0,0 +1,67 @@
+module Imagebinder
+  module ImagebinderHelper
+
+    def ctrl_upload object, asset_type, asset_field=nil
+      asset_field = asset_field || object.class.to_s.underscore
+      link_to('Change', "#" << html_id(asset_field, asset_type), 
+              :id => "link_to_" << html_id(asset_field,asset_type),
+              :class => :'delete-btn'
+             )
+    end
+
+    def ctrl_remove object, asset_type, asset_field=nil
+      asset_field = asset_field || object.class.to_s.underscore
+      link_to_function('Remove', "if (confirm('Do you really want to do this?')) asset_remove('#{html_id(object.class.to_s.underscore, asset_type)}',
+                       '/assets/default/#{asset_field}_#{asset_type}_thumb.png' )", 
+                       :id => "remove_asset_" << html_id(asset_field,asset_type),
+                       :class => :'delete-btn'
+                      )
+    end
+
+    def bind_image object, form, asset_type, *args
+      args = args.first
+      field_label = args[:label] || ''
+      description = args[:description] || ''
+      asset_field = args[:asset_field] || object.class.to_s.underscore
+      style       = args[:style] || :thumb
+
+      object.send('build_'<<asset_type.to_s)  if object.send(asset_type).nil?
+      r = form.fields_for asset_type do |builder|
+        render 'imagebinder/asset_fields', :object => object,
+                                           :f => builder,
+                                           :asset_type  => asset_type,
+                                           :asset_field => asset_field,
+                                           :field_label => field_label,
+                                           :description => description,
+                                           :box_id      => html_id(asset_field, asset_type),
+                                           :style       => style
+      end
+      upload_form = upload_image_form object, asset_type, description, asset_field
+      js = javascript_tag <<-jsinitcode
+        $(function() {
+          $('.asset.#{html_id(asset_field, asset_type)}').closest('form').after("#{escape_javascript(upload_form)}");
+          asset_upload_init($('##{html_id(asset_field, asset_type)}'));
+        });
+      jsinitcode
+      r << js
+    end
+
+
+
+    private
+
+
+    def html_id asset_field, asset_type
+      "#{asset_field}_#{asset_type.to_s}"
+    end
+
+    def upload_image_form object, asset_type, description = '', asset_field=''
+      render 'imagebinder/upload_asset', :object => object.class.new, 
+                                         :asset_type => asset_type, 
+                                         :box_id => html_id(asset_field, asset_type), 
+                                         :description => description,
+                                         :asset_field => asset_field
+    end
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :imagebinders, :only => [:create, :update] do
+  resources :imagebinders, :only => [:create, :update], :controller => "imagebinder/imagebinders" do
     member do
       get :crop
     end
