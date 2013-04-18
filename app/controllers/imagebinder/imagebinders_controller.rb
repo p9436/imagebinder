b33351276230b4fab1345c3988825734c70324d5
Vadym Lukavyy <vadym@masterofcode.com>
2013-04-19 00:28:44 +0300

    Controller

---
+++ app/controllers/imagebinder/imagebinders_controller.rb
@@ -0,0 +1,35 @@
+module Imagebinder
+class ImagebindersController < ApplicationController
+  def create
+    object = params[:asset][:assetable_type].constantize.new
+    asset = object.send('build_' << params[:asset][:association_type], params[:asset])
+    ratio = eval("#{asset.assetable_type}.#{asset.association_type}_ratio")
+
+    if asset.save
+      render :json => { :id => asset.id, :url => crop_imagebinder_url(asset), :crop_ratio => ratio }
+    else
+      render :json => asset.error_messages
+    end
+  end
+
+  def crop
+    @asset = Imgbinder.find params[:id]
+    render 'imagebinder/crop', :layout => false
+  end
+
+
+  def update
+    @asset = Imgbinder.find(params[:id])
+    if @asset.update_attributes(params[:asset])
+      @asset.image.reprocess!
+      asset_field_name = @asset.asset_field.present? ? @asset.asset_field : @asset.assetable_type.underscore
+      render :json => { :id => @asset.id,
+                        :image => @asset.image.url(:thumb),
+                        :target => asset_field_name.to_s << '_' << @asset.association_type }
+    else
+      render 'imagebinder/crop', :layout => false
+    end
+  end
+
+end
+end
--- lib/imagebinder/engine.rb
+++ lib/imagebinder/engine.rb
@@ -1,4 +1,11 @@
+require 'imagebinder'
+require 'rails'
+require 'action_controller'
+# require 'application_helper'
+
+
 module Imagebinder
   class Engine < Rails::Engine
+    # config.autoload_paths << File.expand_path("../lib/some/path", __FILE__)
   end
 end
