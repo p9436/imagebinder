b0172fb8d4c4149fd4d0034ee979ebb347f068c0
Vadym Lukavyy <vadym@masterofcode.com>
2015-01-28 18:34:03 +0200

    Updates

--- lib/imagebinder/acts_as_imagebinder.rb
+++ lib/imagebinder/acts_as_imagebinder.rb
@@ -1,37 +1,51 @@
-module ActsAsImagebinder
-  module ClassMethods
+module Imgbinder
+  module ActsAsImagebinder
+    extend ActiveSupport::Concern
 
-    def acts_as_imagebinder association_name, *args
+    included do
+    end
 
-      has_one association_name, -> { where association_type: association_name},
-              as: :assetable,
-              dependent: :destroy,
-              class_name: 'Imagebinder::Imgbinder'
+    module ClassMethods
 
-      accepts_nested_attributes_for association_name, :allow_destroy => true
+      def acts_as_imagebinder association_name, *args
 
-      args = args.first || {}
-      define_singleton_method "#{association_name.to_s}_styles" do
-        args[:styles] || {}
-      end
 
-      define_singleton_method "#{association_name.to_s}_ratio" do
-        args[:ratio]
-      end
+        has_one association_name, -> { where association_type: association_name},
+                as: :assetable,
+                dependent: :destroy,
+                class_name: 'Imagebinder::Imgbinder'
 
-      define_method "#{association_name.to_s}_attributes=" do |params|
-        if [1, '1', true, 'true'].include? params['_destroy']
-          self.cover = nil
-        else
-          eval("self.#{association_name} = Imagebinder::Imgbinder.find_by_id(params['id'])")
+        accepts_nested_attributes_for association_name, :allow_destroy => true
+
+        define_method association_name.to_s do
+          super().nil? ? eval("build_#{association_name}") : super()
         end
+
+        args = args.first || {}
+        define_singleton_method "#{association_name.to_s}_styles" do
+          args[:styles] || {}
+        end
+
+        define_singleton_method "#{association_name.to_s}_ratio" do
+          args[:ratio]
+        end
+
+        define_method "#{association_name.to_s}_attributes=" do |params|
+          if [1, '1', true, 'true'].include? params['_destroy']
+            self.cover = nil
+          else
+            eval("self.#{association_name} = Imagebinder::Imgbinder.find_by_id(params['id'])")
+          end
+        end
+
       end
 
     end
-  end
 
-  def self.included(base)
-    base.extend(ClassMethods)
-  end
 
+
+
+    ActiveRecord::Base.send :include, Imgbinder::ActsAsImagebinder
+
+  end
 end
